fc5aadf8cdfad05e3fefea16ac07de320eb911a6
Mu'hammad Alabadsa <42316204+mAbadsa@users.noreply.github.com>
2020-05-29 16:52:42 +0300

    Add createIndicators Function

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,6 @@
+const $questionsCount = document.querySelector(".count span");
+const $indicatorsContainer = document.querySelector(".bullets .spans");
+
 function getQuestions() {
   let myRequest = new XMLHttpRequest();
 
@@ -5,11 +8,27 @@ function getQuestions() {
     if (this.readyState === 4 && this.status === 200) {
       const questionsObj = JSON.parse(this.responseText);
       console.log(questionsObj);
+      createIndicators(questionsObj.length);
     }
   };
 
   myRequest.open("GET", "html_questions.json", true);
   myRequest.send();
+}
+
+function createIndicators(num) {
+  $questionsCount.innerHTML = num;
+
+  for (let i = 0; i < num; i++) {
+    // Create indecator
+    let indicator = document.createElement("span");
+
+    if (i === 0) {
+      indicator.className = "on";
+    }
+    // Append indicators to main indicators container
+    $indicatorsContainer.appendChild(indicator);
+  }
 }
 
 getQuestions();
