d2762d1b5f4fcfb0a7775647177521d6bb0c42e9
Mu'hammad Alabadsa <42316204+mAbadsa@users.noreply.github.com>
2020-05-31 01:53:12 +0300

    Show Result On DOM

--- assets/main.js
+++ assets/main.js
@@ -1,11 +1,15 @@
 const $questionsCount = document.querySelector(".count span");
+const $indicators = document.querySelector(".bullets");
 const $indicatorsContainer = document.querySelector(".bullets .spans");
 const $quizArea = document.querySelector(".quiz-area");
 const $answerArea = document.querySelector(".answers-area");
 const $submitButton = document.querySelector(".submit-button");
+const $resultsContainer = document.querySelector(".results");
+const $countDownContainer = document.querySelector(".countdown");
 
 let currentIndex = 0;
 let rightAnswersCount = 0;
+let countDownInterval;
 
 function getQuestions() {
   let myRequest = new XMLHttpRequest();
@@ -23,8 +27,7 @@ function getQuestions() {
 
       $submitButton.addEventListener("click", (e) => {
         let rightAnswer = questionsObj[currentIndex].right_answer;
-        console.log(rightAnswer);
-        console.log(currentIndex);
+
         // Go to Next question
         currentIndex++;
 
@@ -37,7 +40,13 @@ function getQuestions() {
 
         // Handle Indicators Class
         handleIndicatorsClass();
+
+        // Show Result
+        showResult(questionsCount);
+
+        // Count Down
       });
+      countDown(10, questionsCount);
     }
   };
 
@@ -46,7 +55,7 @@ function getQuestions() {
 }
 
 function createIndicators(num) {
-  $questionsCount.innerHTML = num;
+  $questionsCount.innerHTML = num + 1;
 
   for (let i = 0; i < num; i++) {
     // Create indecator
@@ -61,56 +70,57 @@ function createIndicators(num) {
 }
 
 // Add Question To Dom
-function addQuestions(questions, numberOgQuestions) {
-  // Create Questions Head
-  let $questionsTitle = document.createElement("h3");
+function addQuestions(questions, numberOfQuestions) {
+  if (currentIndex < numberOfQuestions) {
+    // Create Questions Head
+    let $questionsTitle = document.createElement("h3");
 
-  // Create Question Text
-  let questionText = document.createTextNode(questions["title"]);
+    // Create Question Text
+    let questionText = document.createTextNode(questions["title"]);
 
-  // Append Question Title Text to Question Title
-  $questionsTitle.appendChild(questionText);
+    // Append Question Title Text to Question Title
+    $questionsTitle.appendChild(questionText);
 
-  //Append Question title to Quiz Area Elemnet
-  $quizArea.appendChild($questionsTitle);
+    //Append Question title to Quiz Area Elemnet
+    $quizArea.appendChild($questionsTitle);
 
-  for (let i = 1; i <= 4; i++) {
-    // Create Container Div for Answer
-    let $answersContainer = document.createElement("div");
+    for (let i = 1; i <= 4; i++) {
+      // Create Container Div for Answer
+      let $answersContainer = document.createElement("div");
 
-    // Add answer Class To answersContainer div
-    $answersContainer.className = "answer";
+      // Add answer Class To answersContainer div
+      $answersContainer.className = "answer";
 
-    // Create Radio Button
-    let $radioButton = document.createElement("input");
+      // Create Radio Button
+      let $radioButton = document.createElement("input");
 
-    // Add type, name and id to radio button
-    $radioButton.name = "question";
-    $radioButton.type = "radio";
-    $radioButton.id = `answer_${i}`;
-    $radioButton.dataset.answer = questions[`answer_${i}`];
+      // Add type, name and id to radio button
+      $radioButton.name = "question";
+      $radioButton.type = "radio";
+      $radioButton.id = `answer_${i}`;
+      $radioButton.dataset.answer = questions[`answer_${i}`];
 
-    i === 1 ? ($radioButton.checked = true) : false;
+      i === 1 ? ($radioButton.checked = true) : false;
 
-    // Create Label
-    let $theLabel = document.createElement("label");
+      // Create Label
+      let $theLabel = document.createElement("label");
 
-    // Add For Attribute to The label
-    $theLabel.htmlFor = `answer_${i}`;
+      // Add For Attribute to The label
+      $theLabel.htmlFor = `answer_${i}`;
 
-    // Create Label Text
-    let labelText = document.createTextNode(questions[`answer_${i}`]);
+      // Create Label Text
+      let labelText = document.createTextNode(questions[`answer_${i}`]);
 
-    $theLabel.appendChild(labelText);
+      $theLabel.appendChild(labelText);
 
-    // Add Input + Label to Container Div
-    $answersContainer.appendChild($radioButton);
-    $answersContainer.appendChild($theLabel);
+      // Add Input + Label to Container Div
+      $answersContainer.appendChild($radioButton);
+      $answersContainer.appendChild($theLabel);
 
-    // Add The Answer Container Div to Answer Area Div
-    $answerArea.appendChild($answersContainer);
+      // Add The Answer Container Div to Answer Area Div
+      $answerArea.appendChild($answersContainer);
+    }
   }
-  //   $quizArea.appendChild;
 }
 
 function checkAnswer(rightAnswer, questionsCount) {
@@ -122,11 +132,43 @@ function checkAnswer(rightAnswer, questionsCount) {
       theChoosenAnswer = answer[i].dataset.answer;
     }
   }
-
-  console.log(rightAnswer);
-  console.log(theChoosenAnswer);
   if (rightAnswer === theChoosenAnswer) {
     rightAnswersCount++;
+  }
+}
+
+function handleIndicatorsClass() {
+  const $indicatorSpan = document.querySelectorAll(".bullets .spans span");
+  const arrayOfSpan = Array.from($indicatorSpan);
+  arrayOfSpan.forEach((span, index) => {
+    if (currentIndex === index) span.className = "on";
+  });
+}
+
+function showResult(questionCount) {
+  let theResults;
+
+  if (currentIndex === questionCount) {
+    $quizArea.remove();
+    $answerArea.remove();
+    $submitButton.remove();
+    $indicators.remove();
+
+    if (
+      rightAnswersCount > questionCount / 2 &&
+      rightAnswersCount < questionCount
+    ) {
+      theResults = `<span class="good">Good</span> ${rightAnswersCount} From ${questionCount}.`;
+    } else if (rightAnswersCount === questionCount) {
+      theResults = `<span class="perfect">Perfect</span> All Answer Is Correct.`;
+    } else {
+      theResults = `<span class="bad">Bad</span> ${rightAnswersCount} From ${questionCount}.`;
+    }
+    // const textNode = document.createTextNode('')
+    $resultsContainer.innerHTML = theResults;
+    $resultsContainer.style.padding = "10px";
+    $resultsContainer.style.backgroundColor = "white";
+    $resultsContainer.style.marginTop = "10px";
   }
 }
 
